3206eeb55450f5156aa72e735cea8e55254aca5e
Siva <siva@znapapp.com>
2022-10-16 01:11:08 +0530

    first commit

---
+++ js/script_custom.js
@@ -0,0 +1,213 @@
+var i = 0;
+
+function progress1() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("progress-bar-1");
+        var width = 1;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (width >= 98) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
+j = 0;
+
+function progress2() {
+    if (j == 0) {
+        j = 1;
+        var elem = document.getElementById("progress-bar-2");
+        var width = 1;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (width >= 53) {
+                clearInterval(id);
+                j = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
+k = 0;
+
+function progress3() {
+    if (k == 0) {
+        k = 1;
+        var elem = document.getElementById("progress-bar-3");
+        var width = 1;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (width >= 31) {
+                clearInterval(id);
+                k = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
+m = 0;
+
+function progress4() {
+    if (m == 0) {
+        m = 1;
+        var elem = document.getElementById("progress-bar-4");
+        var width = 1;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (width >= 15) {
+                clearInterval(id);
+                m = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
+a = 0;
+
+function progress5() {
+    if (a == 0) {
+        a = 1;
+        var elem = document.getElementById("progress-bar-11");
+        var width = 1;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (width >= 91) {
+                clearInterval(id);
+                a = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
+b = 0;
+
+function progress6() {
+    if (b == 0) {
+        b = 1;
+        var elem = document.getElementById("progress-bar-22");
+        var width = 1;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (width >= 90) {
+                clearInterval(id);
+                b = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
+c = 0;
+
+function progress7() {
+    if (c == 0) {
+        c = 1;
+        var elem = document.getElementById("progress-bar-33");
+        var width = 1;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (width >= 85) {
+                clearInterval(id);
+                c = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
+d = 0;
+
+function progress8() {
+    if (d == 0) {
+        d = 1;
+        var elem = document.getElementById("progress-bar-44");
+        var width = 1;
+        var id = setInterval(frame, 10);
+
+        function frame() {
+            if (width >= 54) {
+                clearInterval(id);
+                d = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
+
+progress1();
+
+progress2();
+
+progress3();
+
+progress4();
+progress5();
+progress6();
+progress7();
+progress8();
+window.onload = function() {
+
+    var options = {
+        exportEnabled: true,
+        animationEnabled: true,
+
+        data: [{
+            type: "splineArea",
+            dataPoints: [{
+                    y: 10
+                },
+                {
+                    y: 6
+                },
+                {
+                    y: 14
+                },
+                {
+                    y: 12
+                },
+                {
+                    y: 19
+                },
+                {
+                    y: 14
+                },
+                {
+                    y: 26
+                },
+                {
+                    y: 10
+                },
+                {
+                    y: 22
+                }
+            ]
+        }]
+    };
+    $("#chartContainer").CanvasJSChart(options);
+
+}
